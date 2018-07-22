34ce5b37dc72f5b799de8e5f33f87a12d4638cdd
Anton Tuzhik <TuzhikAA@gmail.com>
2018-07-22 20:35:22 +0300

    fix misspelling

--- routes/tableRoutes.js
+++ routes/tableRoutes.js
@@ -1,4 +1,4 @@
-const requireLogin = require('..  /middlewares/requireLogin');
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
   app.get('/table', requireLogin, (req, res) => {
